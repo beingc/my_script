21b467c6c159abeb9cd3841539ae6fd6907e4a89
x <xuyang.x@qq.com>
2021-05-01 23:09:19 +0800

    add multi thread to speed up

--- script/get_github_ip.py
+++ script/get_github_ip.py
@@ -4,37 +4,38 @@
 
 import requests
 import re
-import time
+import threading
 
 
-def get_html_text(url):
+class MyThread(threading.Thread):
+    def __init__(self, site):
+        threading.Thread.__init__(self)
+        self.site = site
+
+    def run(self):
+        get_site_ip(self.site)
+
+
+def get_site_ip(site):
+    search_url = 'https://www.ipaddress.com/search/'
+    url = search_url + site
     hd = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'}
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/89.0.4389.128 Safari/537.36'}
     r = requests.get(url, headers=hd)
     r.raise_for_status()
     r.encoding = r.apparent_encoding
-    return r.text
-
-
-def bs_parse_ip2(html):
-    url = re.findall(r'ipv4/[\d\.]+', html)
+    url = re.findall(r'ipv4/[\d\.]+', r.text)
     ip = str(url[0]).split('/')[1]
-    return ip
+    print('{} {}'.format(ip, site))
 
 
 def main():
-    search_url = 'https://www.ipaddress.com/search/'
     site_list = ['github.com', 'assets-cdn.github.com', 'github.global.ssl.fastly.net']
     for site in site_list:
-        url = search_url + site
-        html = get_html_text(url)
-        ip = bs_parse_ip2(html)
-        print('{} {}'.format(ip, site))
+        t = MyThread(site)
+        t.start()
 
 
 if __name__ == '__main__':
-    start_time = time.time()
     main()
-    end_time = time.time()
-    print("## Used {:.2f} seconds".format(end_time - start_time))
-    # todo: improve the script to save time
